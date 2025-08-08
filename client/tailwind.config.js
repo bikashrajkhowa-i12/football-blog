09d18cd6d7d2b0739ea28f5f185c57f354356e84
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2025-08-08 14:57:22 +0530

    feat: :art: minor fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-  html: {
-    "scroll-behavior": "smooth",
-  },
   theme: {
     extend: {},
   },
