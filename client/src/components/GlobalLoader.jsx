d3f5db040cb0af8cb2eb3ce57db2c87f7ab54236
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2025-08-12 18:38:38 +0530

    feat: :sparkles: Added "prompt" on the global loader + loader-context

--- client/src/components/GlobalLoader.jsx
+++ client/src/components/GlobalLoader.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useFullInteractionLock } from "../hooks/useFullInteractionLock";
 
-const GlobalLoader = ({ loading = false }) => {
+const GlobalLoader = ({ loading = false, prompt = "Loading..." }) => {
   const [renderLoading, setRenderLoading] = useState(false);
   const [visibleLoader, setVisibleLoader] = useState(false);
 
@@ -28,8 +28,8 @@ const GlobalLoader = ({ loading = false }) => {
 
   return (
     <div
-      className={`fixed inset-0 z-[9999] flex items-center justify-center 
-        bg-black bg-opacity-70 transition-opacity duration-300
+      className={`fixed inset-0 z-[9999] flex flex-col items-center justify-center 
+        bg-black bg-opacity-70 transition-opacity duration-300 gap-10
         ${visibleLoader ? "opacity-100" : "opacity-0"}`}
     >
       {/* Loader */}
@@ -37,6 +37,7 @@ const GlobalLoader = ({ loading = false }) => {
         className="w-12 h-12 md:w-20 md:h-20 border-4 border-white border-t-transparent rounded-full animate-spin"
         aria-hidden="true"
       />
+      <p className="text-sky-500">{prompt}</p>
     </div>
   );
 };
--- client/src/context/LoaderContext.jsx
+++ client/src/context/LoaderContext.jsx
@@ -6,14 +6,15 @@ export const LoaderProvider = ({ children }) => {
   const [loaderState, setLoaderState] = useState({
     type: null, //global is mandatory, custom ++ for contentLoader
     loading: false,
+    prompt: "Loading...",
   });
 
   const startLoading = (type = "global") => {
-    setLoaderState({ type, loading: true });
+    setLoaderState({ type, loading: true, prompt: "Loading..." });
   };
 
   const stopLoading = (type = "global") => {
-    setLoaderState({ type, loading: false });
+    setLoaderState({ type, loading: false, prompt: "Loading..." });
   };
 
   return (
